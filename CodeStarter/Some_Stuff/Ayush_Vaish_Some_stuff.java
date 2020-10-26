b60cfd9d4b02dca44b9d201a0fb9ca753bc302d9
ayushvaish2511 <72246792+ayushvaish2511@users.noreply.github.com>
2020-10-26 21:09:47 +0530

    Create Ayush_Vaish_Some_stuff.java
    Some stuff problem using scanner class in java

---
+++ CodeStarter/Some_Stuff/Ayush_Vaish_Some_stuff.java
@@ -0,0 +1,17 @@
+//Some stuff problem using scanner class in java
+
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i<=n; i++)
+        {
+            for(int j = 1; j<=i; j++)
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
