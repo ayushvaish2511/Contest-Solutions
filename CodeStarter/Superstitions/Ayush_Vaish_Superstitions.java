30dc9fa69332dfbfcf0ae5757f35c92e8df993ed
ayushvaish2511 <72246792+ayushvaish2511@users.noreply.github.com>
2020-10-26 21:26:35 +0530

    Create Ayush_Vaish_Superstitions.java
    Solving Superstition problem in java using scanner class

---
+++ CodeStarter/Superstitions/Ayush_Vaish_Superstitions.java
@@ -0,0 +1,26 @@
+//Solving Superstition problem in java using scanner class
+
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int a[] = new int[N];
+        for(int i = 0; i<N; i++)
+        a[i] = sc.nextInt();
+        int c = 0;
+        for(int i = 0; i<N-1; i++)
+        {
+            for(int j = i+1; j<N; j++)
+            {
+                if((a[i]+a[j])==X)
+                c++;
+               
+            }
+        }
+        System.out.println(c);
+    }
+}
