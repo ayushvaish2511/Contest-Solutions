a0274916af3d65771c041924cc7e6c21c5c8215a
ayushvaish2511 <72246792+ayushvaish2511@users.noreply.github.com>
2020-10-28 21:17:22 +0530

    Create Ayush_Vaish_DrunkLover.java
    To solve Drunk lover problem in java using scanner class

---
+++ CodeStarter/Drunk_Lover/Ayush_Vaish_DrunkLover.java
@@ -0,0 +1,29 @@
+//To solve Drunk lover problem in java using scanner class
+
+import java.util.*;
+class Solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int q = sc.nextInt();
+        while(q!=0)
+        {
+            long a = sc.nextLong();
+            long b = sc.nextLong();
+            long t = sc.nextLong();
+            long sum = 0;
+            if(t%2==0)
+            {
+                sum = (t/2)*(a-b);
+            }
+            else
+            {
+                sum = (t/2)*(a-b);
+                sum = sum + a;
+            }
+            System.out.println(sum);
+            q--;
+        }
+    }
+}
